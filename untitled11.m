65f7d542e9168a066855d229d67bc354cdaf200e
Di Wang <35476470+DrGabor@users.noreply.github.com>
2019-01-27 13:18:24 -0800

    Add files via upload

---
+++ untitled11.m
@@ -0,0 +1,43 @@
+clc; close all; 
+DataRoot = 'C:\Users\lucaswang\Documents\Visual Studio 2015\Projects\reLocalizer\reLocalizer\output';
+DataDir = fullfile(DataRoot, 'cloud_mov.pcd'); 
+cloud_mov = pcread(DataDir); 
+DataDir = fullfile(DataRoot, 'cloud_ref.pcd'); 
+cloud_ref = pcread(DataDir); 
+figure; 
+hold on; 
+grid on; 
+pcshow(cloud_mov); 
+pcshow(cloud_ref.Location, 'b'); 
+DataDir = fullfile(DataRoot, 'keypoints_mov.pcd'); 
+cloud_key_points_mov = pcread(DataDir); 
+pcshow(cloud_key_points_mov.Location, 'r', 'markersize', 100); 
+DataDir = fullfile(DataRoot, 'keypoints_ref.pcd'); 
+cloud_key_points_ref = pcread(DataDir); 
+pcshow(cloud_key_points_ref.Location, 'k', 'markersize', 100); 
+%% 
+DataDir = fullfile(DataRoot, 'coarse_results.txt'); 
+A = importdata(DataDir);
+str = A.textdata;
+str = strtrim(str);
+Headers = split(str);
+[~, pos_t] = ismember({'tx', 'ty', 'tz'}, Headers);
+[~, pos_q] = ismember({'qw', 'qx', 'qy', 'qz'}, Headers);
+
+data = A.data; 
+dT = data(pos_t)'; 
+quat = data(pos_q); 
+dR = quat2rotm(quat);
+aft = Loc2Glo(cloud_mov.Location', dR', dT); 
+
+figure; 
+hold on; 
+grid on; 
+axis equal; 
+xlabel('X/m'); 
+ylabel('Y/m'); 
+pcshow(cloud_ref.Location, 'g', 'markersize', 100); 
+pcshow(cloud_mov.Location, 'r', 'markersize', 100); 
+pcshow(aft', 'b', 'markersize', 100); 
+legend('Ref', 'Mov', 'Coarse Aligned');
+legend('boxoff'); 
